f47c5205906195f19b29e38b42496250576b942a
Danilo Pianini <danilo.pianini@gmail.com>
2021-05-21 16:18:29 +0200

    Restructure the document

---
+++ scholar_scraper.rb
@@ -0,0 +1,42 @@
+#! /usr/bin/env ruby
+require 'nokogiri'
+require 'net/http'
+require 'pathname'
+
+def download_html(url)
+    uri = URI.parse(url)
+    response = Net::HTTP.get(uri)
+    Nokogiri::HTML(response)
+end
+
+#Scholar
+scholar_page = download_html('https://scholar.google.com/citations?user=0CLlt5oAAAAJ&hl=en')
+since = scholar_page.xpath('//*[@class="gsc_rsb_sth"]')
+    .map { |tag| tag.text }
+    .filter { |text| text.include?('Since') }
+    .first
+citedBy = scholar_page.xpath('//*[@class="gsc_rsb_std"]')
+    .map {| tag | tag.text }
+latex_newline = '\\\\'
+tex = <<-TeX
+\\textbf{\\href{https://scholar.google.com/citations?user=0CLlt5oAAAAJ&hl=en}{Google Scholar metrics as of #{Time.now.strftime('%Y-%m-%d')}}}
+\\begin{outerlist}
+    \\item[] Overall
+    \\begin{innerlist}
+        \\item Citations: #{citedBy[0]}
+        \\item h-Index: #{citedBy[2]}
+        \\item i10-Index: #{citedBy[4]}
+    \\end{innerlist}
+    \\item[] #{since}
+    \\begin{innerlist}
+        \\item Citations: #{citedBy[1]}
+        \\item h-Index: #{citedBy[3]}
+        \\item i10-Index: #{citedBy[5]}
+    \\end{innerlist}
+\\end{outerlist}
+TeX
+puts tex
+File.write('scholar.tex', tex)
+
+#Scopus
+puts download_html('https://www.scopus.com/authid/detail.uri?authorId=36997527800')
