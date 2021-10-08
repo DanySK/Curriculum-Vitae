f9b879eeb9e9e42f36c455c493eccdc596bf5a69
Danilo Pianini <danilo.pianini@unibo.it>
2021-10-08 12:08:03 +0200

    Update setup_ruby.sh

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
+echo Setting up Ruby
 if [ "$CI" = 'true' ]; then
+  echo Detected the CI environment, installing bundler with sudo
   sudo gem install bundler
-else
-  gem install --user-install bundler
 fi
+echo Installing bundler as user
+gem install --user-install bundler
+echo Running bundle install
 bundle install
